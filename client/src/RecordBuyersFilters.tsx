378700851191cc81d94d6dfa966bf18c78334fa5
SawdaDevs <sawda.onanuga@gmail.com>
2023-10-25 22:46:51 +0100

    added select tool

---
+++ client/src/RecordBuyersFilters.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import type { SelectProps } from 'antd';
+import { Select } from 'antd';
+
+const options: SelectProps['options'] = [];
+
+for (let i = 0; i < 100000; i++) {
+  const value = `${i.toString(36)}${i}`;
+  options.push({
+    label: value,
+    value,
+    disabled: i === 10,
+  });
+}
+
+const handleChange = (value: string[]) => {
+  console.log(`selected ${value}`);
+};
+
+export function RecordBuyersFilters(){
+
+    return (
+        <>
+            <Select
+            mode="multiple"
+            style={{ width: '50%' }}
+            placeholder="Filter Buyer"
+            defaultValue={[]}
+            onChange={handleChange}
+            options={options}
+            />
+        </>
+    );
+}
+  
+
